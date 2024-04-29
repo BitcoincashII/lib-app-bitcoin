LedgerHQ/app-bitcoin

Remove throw, use standard app layout, clean, and move to its own lib

lib-app-bitcoin/customizable_ui.c
@@ -1,0 +1,117 @@
+/*******************************************************************************
+*   Ledger App - Bitcoin Wallet
+*   (c) 2016-2019 Ledger
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+********************************************************************************/
+#include "customizable_ui.h"
+#include "customizable_helpers.h"
+#include "helpers.h"
+#include "context.h"
+#include "display_variables.h"
+#include "segwit_addr.h"
+#include "cashaddr.h"
+#include "be_operations.h"
+#include "display_utils.h"
+#include "read.h"
+
+
+WEAK void get_address_from_output_script(unsigned char* script, int script_size, char* out, int out_size) {
+    if (output_script_is_op_return(script)) {
+        strncpy(out, "OP_RETURN", out_size);
+        return;
+    }
+    if ((COIN_KIND == COIN_KIND_HYDRA) &&
+        output_script_is_op_create(script, script_size)) {
+        strncpy(out, "OP_CREATE", out_size);
+        return;
+    }
+    if ((COIN_KIND == COIN_KIND_HYDRA) &&
+        output_script_is_op_call(script, script_size)) {
+        strncpy(out, "OP_CALL", out_size);
+        return;
+    }
+    if (output_script_is_native_witness(script)) {
+        if (COIN_NATIVE_SEGWIT_PREFIX) {
+            segwit_addr_encode(
+                out, (char *)PIC(COIN_NATIVE_SEGWIT_PREFIX), 0,
+                script + OUTPUT_SCRIPT_NATIVE_WITNESS_PROGRAM_OFFSET,
+                script[OUTPUT_SCRIPT_NATIVE_WITNESS_PROGRAM_OFFSET - 1]);
+        }
+        return;
+    }
+    unsigned char versionSize;
+    unsigned char address[22];
+    unsigned short textSize;
+    int addressOffset = 3;
+    unsigned short version = COIN_P2SH_VERSION;
+
+    if (output_script_is_regular(script)) {
+        addressOffset = 4;
+        version = COIN_P2PKH_VERSION;
+    }
+
+    if (version > 255) {
+        versionSize = 2;
+        address[0] = (version >> 8);
+        address[1] = version;
+    } else {
+        versionSize = 1;
+        address[0] = version;
+    }
+    memmove(address + versionSize, script + addressOffset, 20);
+
+    // Prepare address
+    if (context_D.usingCashAddr) {
+        cashaddr_encode(
+            address + versionSize, 20, (uint8_t *)out, out_size,
+            (version == COIN_P2SH_VERSION
+                    ? CASHADDR_P2SH
+                    : CASHADDR_P2PKH));
+    } else {
+        textSize = public_key_to_encoded_base58(
+            address, 20 + versionSize, (unsigned char *)out,
+            out_size, version, 1);
+        out[textSize] = '\0';
+    }
+}
+
+WEAK uint8_t prepare_fees(void) {
+    if (context_D.transactionContext.relaxed) {
+        memmove(vars.tmp.feesAmount, "UNKNOWN", 7);
+        vars.tmp.feesAmount[7] = '\0';
+    } else {
+        unsigned char fees[8];
+        unsigned char borrow;
+
+        borrow = transaction_amount_sub_be(
+                fees, context_D.transactionContext.transactionAmount,
+                context_D.totalOutputAmount);
+        if (borrow && COIN_KIND == COIN_KIND_KOMODO) {
+            memmove(vars.tmp.feesAmount, "REWARD", 6);
+            vars.tmp.feesAmount[6] = '\0';
+        }
+        else {
+            if (borrow) {
+                PRINTF("Error : Fees not consistent");
+                goto error;
+            }
+        format_sats_amount(COIN_COINID_SHORT,
+                (uint64_t)read_u64_be(fees, 0), // Cast prevents weird compilo bug
+                vars.tmp.feesAmount);
+        }
+    }
+    return 1;
+error:
+    return 0;
+}

lib-app-bitcoin/filesystem.h
@@ -1,0 +1,40 @@
+/*******************************************************************************
+*   Ledger App - Bitcoin Wallet
+*   (c) 2016-2019 Ledger
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+********************************************************************************/
+
+#pragma once 
+
+#include "os.h"
+#include "context.h"
+#include "filesystem_tx.h"
+
+enum family_e {
+    FAMILY_BITCOIN = 0x01,
+    FAMILY_PEERCOIN = 0x02,
+    FAMILY_STEALTH = 0x04
+};
+
+typedef struct backup_area_s {
+    uint8_t trustedinput_key[32];
+} backup_area_t;
+
+typedef struct storage_s {
+    backup_area_t bkp;
+} storage_t;
+
+// the global nvram memory variable
+extern storage_t const N_real;
+#define N_btchip (*(volatile storage_t *)PIC(&N_real))

lib-app-bitcoin/handler/get_trusted_input.c
@@ -1,0 +1,89 @@
+/*******************************************************************************
+*   Ledger App - Bitcoin Wallet
+*   (c) 2016-2019 Ledger
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+********************************************************************************/
+#include "lib_standard_app/read.h"
+#include "lib_standard_app/write.h"
+#include "io.h"
+
+#include "context.h"
+#include "filesystem.h"
+#include "transaction.h"
+#include "apdu_constants.h"
+
+#define GET_TRUSTED_INPUT_P1_FIRST 0x00
+#define GET_TRUSTED_INPUT_P1_NEXT 0x80
+
+WEAK unsigned short handler_get_trusted_input(buffer_t* buffer, uint8_t p1, uint8_t p2) {
+    unsigned char dataOffset = 0;
+
+    if (p1 == GET_TRUSTED_INPUT_P1_FIRST) {
+        // Initialize
+        context_D.transactionTargetInput =
+            read_u32_be(buffer->ptr, 0);
+        context_D.transactionContext.transactionState =
+            TRANSACTION_NONE;
+        context_D.trustedInputProcessed = 0;
+        context_D.transactionContext.consumeP2SH = 0;
+        dataOffset = 4;
+        context_D.transactionHashOption = TRANSACTION_HASH_FULL;
+        context_D.usingSegwit = 0;
+        context_D.usingOverwinter = 0;
+    } else if (p1 != GET_TRUSTED_INPUT_P1_NEXT) {
+        return io_send_sw(SW_INCORRECT_P1_P2);
+    }
+
+    if (p2 != 0x00) {
+        return io_send_sw(SW_INCORRECT_P1_P2);
+    }
+
+    context_D.transactionBufferPointer = (uint8_t* ) buffer->ptr + dataOffset;
+    context_D.transactionDataRemaining = buffer->size - dataOffset;
+
+    transaction_parse(PARSE_MODE_TRUSTED_INPUT);
+
+    if (context_D.transactionContext.transactionState ==
+        TRANSACTION_PARSED) {
+
+        context_D.transactionContext.transactionState =
+            TRANSACTION_NONE;
+        if (!context_D.trustedInputProcessed) {
+            // Output was not found
+            return io_send_sw(SW_INCORRECT_DATA);
+        }
+
+        if (cx_hash_no_throw(&context_D.transactionHashFull.sha256.header, CX_LAST,
+                NULL, 0, G_io_apdu_buffer + TRUSTED_INPUT_SIZE, 32)) {
+            return io_send_sw(SW_TECHNICAL_PROBLEM);
+        }
+
+        // Otherwise prepare
+        cx_rng(G_io_apdu_buffer, 8);
+        G_io_apdu_buffer[0] = MAGIC_TRUSTED_INPUT;
+        G_io_apdu_buffer[1] = 0x00;
+        cx_hash_sha256(G_io_apdu_buffer + TRUSTED_INPUT_SIZE, 32, G_io_apdu_buffer + 4, 32);
+
+        write_u32_le(G_io_apdu_buffer, 4 + 32,
+                            context_D.transactionTargetInput);
+        memmove(G_io_apdu_buffer + 4 + 32 + 4,
+                   context_D.transactionContext.transactionAmount, 8);
+
+        cx_hmac_sha256((uint8_t *)N_btchip.bkp.trustedinput_key,
+                       sizeof(N_btchip.bkp.trustedinput_key), G_io_apdu_buffer,
+                       TRUSTED_INPUT_SIZE, G_io_apdu_buffer + TRUSTED_INPUT_SIZE, 32);
+        context_D.outLength = TRUSTED_INPUT_TOTAL_SIZE;
+    }
+    return io_send_response_pointer(G_io_apdu_buffer, context_D.outLength, SW_OK);
+}

lib-app-bitcoin/handler/hash_input_start.c
@@ -1,0 +1,136 @@
+/*******************************************************************************
+*   Ledger App - Bitcoin Wallet
+*   (c) 2016-2019 Ledger
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+********************************************************************************/
+
+#include "io.h"
+
+#include "context.h"
+#include "transaction.h"
+#include "apdu_constants.h"
+#include "extensions.h"
+#include "swap.h"
+
+#define P1_FIRST 0x00
+#define P1_NEXT 0x80
+#define P2_NEW 0x00
+#define P2_NEW_SEGWIT 0x02
+#define P2_NEW_SEGWIT_CASHADDR 0x03
+#define P2_NEW_SEGWIT_OVERWINTER 0x04
+#define P2_NEW_SEGWIT_SAPLING 0x05
+#define P2_CONTINUE 0x80
+
+#define IS_INPUT()                                        \
+    (buffer->size - 1 > 8                                 \
+     && buffer->size - 1 <= TRUSTED_INPUT_TOTAL_SIZE + 2  \
+     && buffer->ptr[0] <= 0x02)                           \
+
+#define IS_INPUT_TRUSTED()                           \
+    (buffer->ptr[0] == 0x01                          \
+     && buffer->ptr[1] == TRUSTED_INPUT_TOTAL_SIZE   \
+     && buffer->ptr[2] == MAGIC_TRUSTED_INPUT        \
+     && buffer->ptr[3] == 0x00)
+
+WEAK unsigned short handler_hash_input_start(buffer_t* buffer, uint8_t p1, uint8_t p2) {
+    if (p1 == P1_FIRST) {
+        // Initialize
+        context_D.transactionContext.transactionState =
+            TRANSACTION_NONE;
+        context_D.transactionHashOption = TRANSACTION_HASH_BOTH;
+    } else if (p1 != P1_NEXT) {
+        return io_send_sw(SW_INCORRECT_P1_P2);
+    }
+
+    if ((p2 == P2_NEW) ||
+        (p2 == P2_NEW_SEGWIT) ||
+        (p2 == P2_NEW_SEGWIT_CASHADDR) ||
+        (p2 == P2_NEW_SEGWIT_OVERWINTER) ||
+        (p2 == P2_NEW_SEGWIT_SAPLING)) {
+        if (p1 == P1_FIRST) {
+            unsigned char usingSegwit =
+                (p2 == P2_NEW_SEGWIT) ||
+                (p2 == P2_NEW_SEGWIT_CASHADDR) ||
+                (p2 == P2_NEW_SEGWIT_OVERWINTER) ||
+                (p2 == P2_NEW_SEGWIT_SAPLING);
+            // Master transaction reset
+            context_D.transactionContext.firstSigned = 1;
+            context_D.transactionContext.consumeP2SH = 0;
+            context_D.transactionContext.relaxed = 0;
+            context_D.usingSegwit = usingSegwit;
+
+            if (COIN_KIND == COIN_KIND_BITCOIN_CASH) {
+                unsigned char usingCashAddr = (p2 == P2_NEW_SEGWIT_CASHADDR);
+                context_D.usingCashAddr = usingCashAddr;
+            }
+            else {
+                context_D.usingCashAddr = 0;
+            }
+
+            context_D.usingOverwinter = 0;
+            if ((COIN_KIND == COIN_KIND_ZCASH) || (COIN_KIND == COIN_KIND_KOMODO) || (COIN_KIND == COIN_KIND_ZCLASSIC) || (COIN_KIND == COIN_KIND_RESISTANCE)) {
+                if (p2 == P2_NEW_SEGWIT_OVERWINTER) {
+                    context_D.usingOverwinter = ZCASH_USING_OVERWINTER;
+                }
+                else
+                if (p2 == P2_NEW_SEGWIT_SAPLING) {
+                    context_D.usingOverwinter = ZCASH_USING_OVERWINTER_SAPLING;
+                }
+            }
+            context_D.overwinterSignReady = 0;
+            context_D.segwitParsedOnce = 0;
+            // Initialize for screen pairing
+            memset(&context_D.tmpCtx.output, 0,
+                      sizeof(context_D.tmpCtx.output));
+            context_D.tmpCtx.output.changeAccepted = 1;
+            // Reset segwitWarningSeen flag to prevent displaying the warning for each
+            // segwit input when coontinuing from a previous session (P2=0x80)
+            context_D.segwitWarningSeen = 0;
+        }
+    } else if (p2 != P2_CONTINUE) {
+        return io_send_sw(SW_INCORRECT_P1_P2);
+    }
+
+    // In segwit mode, warn user one time only to update its client wallet...
+    if (context_D.usingSegwit
+        && !context_D.segwitWarningSeen
+        && (p1 == P1_NEXT)
+        && (p2 != P2_CONTINUE)
+        // ...if input is not passed as a TrustedInput
+        && IS_INPUT()
+        && !IS_INPUT_TRUSTED())
+    {
+        if(G_called_from_swap){
+            /* There is no point in displaying a warning when the app is signing
+            in silent mode, as its UI is hidden behind the exchange app*/
+            return io_send_sw(SW_SWAP_WITHOUT_TRUSTED_INPUTS);
+        }
+        context_D.segwitWarningSeen = 1;
+        request_segwit_input_approval();
+        // Start parsing of the 1st chunk
+        context_D.transactionBufferPointer = (uint8_t*) buffer->ptr;
+        context_D.transactionDataRemaining = buffer->size;
+
+        transaction_parse(PARSE_MODE_SIGNATURE);
+        return 0;
+    }
+
+    // Start parsing of the 1st chunk
+    context_D.transactionBufferPointer = (uint8_t*) buffer->ptr;
+    context_D.transactionDataRemaining = buffer->size;
+
+    transaction_parse(PARSE_MODE_SIGNATURE);
+
+    return io_send_response_pointer(G_io_apdu_buffer, context_D.outLength, SW_OK);
+}

BitcoincashII/lib-app-bitcoin

lib-app-bitcoin/customizable_ui.c
@@ -1,0 +1,117 @@
+/*******************************************************************************
+*   Ledger App - Bitcoin Wallet
+*   (c) 2016-2019 Ledger
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+********************************************************************************/
+#include "customizable_ui.h"
+#include "customizable_helpers.h"
+#include "helpers.h"
+#include "context.h"
+#include "display_variables.h"
+#include "segwit_addr.h"
+#include "cashaddr.h"
+#include "be_operations.h"
+#include "display_utils.h"
+#include "read.h"
+
+
+WEAK void get_address_from_output_script(unsigned char* script, int script_size, char* out, int out_size) {
+    if (output_script_is_op_return(script)) {
+        strncpy(out, "OP_RETURN", out_size);
+        return;
+    }
+    if ((COIN_KIND == COIN_KIND_HYDRA) &&
+        output_script_is_op_create(script, script_size)) {
+        strncpy(out, "OP_CREATE", out_size);
+        return;
+    }
+    if ((COIN_KIND == COIN_KIND_HYDRA) &&
+        output_script_is_op_call(script, script_size)) {
+        strncpy(out, "OP_CALL", out_size);
+        return;
+    }
+    if (output_script_is_native_witness(script)) {
+        if (COIN_NATIVE_SEGWIT_PREFIX) {
+            segwit_addr_encode(
+                out, (char *)PIC(COIN_NATIVE_SEGWIT_PREFIX), 0,
+                script + OUTPUT_SCRIPT_NATIVE_WITNESS_PROGRAM_OFFSET,
+                script[OUTPUT_SCRIPT_NATIVE_WITNESS_PROGRAM_OFFSET - 1]);
+        }
+        return;
+    }
+    unsigned char versionSize;
+    unsigned char address[22];
+    unsigned short textSize;
+    int addressOffset = 3;
+    unsigned short version = COIN_P2SH_VERSION;
+
+    if (output_script_is_regular(script)) {
+        addressOffset = 4;
+        version = COIN_P2PKH_VERSION;
+    }
+
+    if (version > 255) {
+        versionSize = 2;
+        address[0] = (version >> 8);
+        address[1] = version;
+    } else {
+        versionSize = 1;
+        address[0] = version;
+    }
+    memmove(address + versionSize, script + addressOffset, 20);
+
+    // Prepare address
+    if (context.usingCashAddr) {
+        cashaddr_encode(
+            address + versionSize, 20, (uint8_t *)out, out_size,
+            (version == COIN_P2SH_VERSION
+                    ? CASHADDR_P2SH
+                    : CASHADDR_P2PKH));
+    } else {
+        textSize = public_key_to_encoded_base58(
+            address, 20 + versionSize, (unsigned char *)out,
+            out_size, version, 1);
+        out[textSize] = '\0';
+    }
+}
+
+WEAK uint8_t prepare_fees(void) {
+    if (context.transactionContext.relaxed) {
+        memmove(vars.tmp.feesAmount, "UNKNOWN", 7);
+        vars.tmp.feesAmount[7] = '\0';
+    } else {
+        unsigned char fees[8];
+        unsigned char borrow;
+
+        borrow = transaction_amount_sub_be(
+                fees, context.transactionContext.transactionAmount,
+                context.totalOutputAmount);
+        if (borrow && COIN_KIND == COIN_KIND_KOMODO) {
+            memmove(vars.tmp.feesAmount, "REWARD", 6);
+            vars.tmp.feesAmount[6] = '\0';
+        }
+        else {
+            if (borrow) {
+                PRINTF("Error : Fees not consistent");
+                goto error;
+            }
+        format_sats_amount(COIN_COINID_SHORT,
+                (uint64_t)read_u64_be(fees, 0), // Cast prevents weird compilo bug
+                vars.tmp.feesAmount);
+        }
+    }
+    return 1;
+error:
+    return 0;
+}

lib-app-bitcoin/filesystem.h
@@ -1,0 +1,40 @@
+/*******************************************************************************
+*   Ledger App - Bitcoin Wallet
+*   (c) 2016-2019 Ledger
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+********************************************************************************/
+
+#pragma once 
+
+#include "os.h"
+#include "context.h"
+#include "filesystem_tx.h"
+
+enum family_e {
+    FAMILY_BITCOIN = 0x01,
+    FAMILY_PEERCOIN = 0x02,
+    FAMILY_STEALTH = 0x04
+};
+
+typedef struct backup_area_s {
+    uint8_t trustedinput_key[32];
+} backup_area_t;
+
+typedef struct storage_s {
+    backup_area_t bkp;
+} storage_t;
+
+// the global nvram memory variable
+extern storage_t const N_real;
+#define g_nvram_data (*(volatile storage_t *)PIC(&N_real))

lib-app-bitcoin/handler/get_trusted_input.c
@@ -1,0 +1,89 @@
+/*******************************************************************************
+*   Ledger App - Bitcoin Wallet
+*   (c) 2016-2019 Ledger
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+********************************************************************************/
+#include "lib_standard_app/read.h"
+#include "lib_standard_app/write.h"
+#include "io.h"
+
+#include "context.h"
+#include "filesystem.h"
+#include "transaction.h"
+#include "apdu_constants.h"
+
+#define GET_TRUSTED_INPUT_P1_FIRST 0x00
+#define GET_TRUSTED_INPUT_P1_NEXT 0x80
+
+WEAK unsigned short handler_get_trusted_input(buffer_t* buffer, uint8_t p1, uint8_t p2) {
+    unsigned char dataOffset = 0;
+
+    if (p1 == GET_TRUSTED_INPUT_P1_FIRST) {
+        // Initialize
+        context.transactionTargetInput =
+            read_u32_be(buffer->ptr, 0);
+        context.transactionContext.transactionState =
+            TRANSACTION_NONE;
+        context.trustedInputProcessed = 0;
+        context.transactionContext.consumeP2SH = 0;
+        dataOffset = 4;
+        context.transactionHashOption = TRANSACTION_HASH_FULL;
+        context.usingSegwit = 0;
+        context.usingOverwinter = 0;
+    } else if (p1 != GET_TRUSTED_INPUT_P1_NEXT) {
+        return io_send_sw(SW_INCORRECT_P1_P2);
+    }
+
+    if (p2 != 0x00) {
+        return io_send_sw(SW_INCORRECT_P1_P2);
+    }
+
+    context.transactionBufferPointer = (uint8_t* ) buffer->ptr + dataOffset;
+    context.transactionDataRemaining = buffer->size - dataOffset;
+
+    transaction_parse(PARSE_MODE_TRUSTED_INPUT);
+
+    if (context.transactionContext.transactionState ==
+        TRANSACTION_PARSED) {
+
+        context.transactionContext.transactionState =
+            TRANSACTION_NONE;
+        if (!context.trustedInputProcessed) {
+            // Output was not found
+            return io_send_sw(SW_INCORRECT_DATA);
+        }
+
+        if (cx_hash_no_throw(&context.transactionHashFull.sha256.header, CX_LAST,
+                NULL, 0, G_io_apdu_buffer + TRUSTED_INPUT_SIZE, 32)) {
+            return io_send_sw(SW_TECHNICAL_PROBLEM);
+        }
+
+        // Otherwise prepare
+        cx_rng(G_io_apdu_buffer, 8);
+        G_io_apdu_buffer[0] = MAGIC_TRUSTED_INPUT;
+        G_io_apdu_buffer[1] = 0x00;
+        cx_hash_sha256(G_io_apdu_buffer + TRUSTED_INPUT_SIZE, 32, G_io_apdu_buffer + 4, 32);
+
+        write_u32_le(G_io_apdu_buffer, 4 + 32,
+                            context.transactionTargetInput);
+        memmove(G_io_apdu_buffer + 4 + 32 + 4,
+                   context.transactionContext.transactionAmount, 8);
+
+        cx_hmac_sha256((uint8_t *)g_nvram_data.bkp.trustedinput_key,
+                       sizeof(g_nvram_data.bkp.trustedinput_key), G_io_apdu_buffer,
+                       TRUSTED_INPUT_SIZE, G_io_apdu_buffer + TRUSTED_INPUT_SIZE, 32);
+        context.outLength = TRUSTED_INPUT_TOTAL_SIZE;
+    }
+    return io_send_response_pointer(G_io_apdu_buffer, context.outLength, SW_OK);
+}

lib-app-bitcoin/handler/hash_input_start.c
@@ -1,0 +1,136 @@
+/*******************************************************************************
+*   Ledger App - Bitcoin Wallet
+*   (c) 2016-2019 Ledger
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+********************************************************************************/
+
+#include "io.h"
+
+#include "context.h"
+#include "transaction.h"
+#include "apdu_constants.h"
+#include "extensions.h"
+#include "swap.h"
+
+#define P1_FIRST 0x00
+#define P1_NEXT 0x80
+#define P2_NEW 0x00
+#define P2_NEW_SEGWIT 0x02
+#define P2_NEW_SEGWIT_CASHADDR 0x03
+#define P2_NEW_SEGWIT_OVERWINTER 0x04
+#define P2_NEW_SEGWIT_SAPLING 0x05
+#define P2_CONTINUE 0x80
+
+#define IS_INPUT()                                        \
+    (buffer->size - 1 > 8                                 \
+     && buffer->size - 1 <= TRUSTED_INPUT_TOTAL_SIZE + 2  \
+     && buffer->ptr[0] <= 0x02)                           \
+
+#define IS_INPUT_TRUSTED()                           \
+    (buffer->ptr[0] == 0x01                          \
+     && buffer->ptr[1] == TRUSTED_INPUT_TOTAL_SIZE   \
+     && buffer->ptr[2] == MAGIC_TRUSTED_INPUT        \
+     && buffer->ptr[3] == 0x00)
+
+WEAK unsigned short handler_hash_input_start(buffer_t* buffer, uint8_t p1, uint8_t p2) {
+    if (p1 == P1_FIRST) {
+        // Initialize
+        context.transactionContext.transactionState =
+            TRANSACTION_NONE;
+        context.transactionHashOption = TRANSACTION_HASH_BOTH;
+    } else if (p1 != P1_NEXT) {
+        return io_send_sw(SW_INCORRECT_P1_P2);
+    }
+
+    if ((p2 == P2_NEW) ||
+        (p2 == P2_NEW_SEGWIT) ||
+        (p2 == P2_NEW_SEGWIT_CASHADDR) ||
+        (p2 == P2_NEW_SEGWIT_OVERWINTER) ||
+        (p2 == P2_NEW_SEGWIT_SAPLING)) {
+        if (p1 == P1_FIRST) {
+            unsigned char usingSegwit =
+                (p2 == P2_NEW_SEGWIT) ||
+                (p2 == P2_NEW_SEGWIT_CASHADDR) ||
+                (p2 == P2_NEW_SEGWIT_OVERWINTER) ||
+                (p2 == P2_NEW_SEGWIT_SAPLING);
+            // Master transaction reset
+            context.transactionContext.firstSigned = 1;
+            context.transactionContext.consumeP2SH = 0;
+            context.transactionContext.relaxed = 0;
+            context.usingSegwit = usingSegwit;
+
+            if (COIN_KIND == COIN_KIND_BITCOIN_CASH) {
+                unsigned char usingCashAddr = (p2 == P2_NEW_SEGWIT_CASHADDR);
+                context.usingCashAddr = usingCashAddr;
+            }
+            else {
+                context.usingCashAddr = 0;
+            }
+
+            context.usingOverwinter = 0;
+            if ((COIN_KIND == COIN_KIND_ZCASH) || (COIN_KIND == COIN_KIND_KOMODO) || (COIN_KIND == COIN_KIND_ZCLASSIC) || (COIN_KIND == COIN_KIND_RESISTANCE)) {
+                if (p2 == P2_NEW_SEGWIT_OVERWINTER) {
+                    context.usingOverwinter = ZCASH_USING_OVERWINTER;
+                }
+                else
+                if (p2 == P2_NEW_SEGWIT_SAPLING) {
+                    context.usingOverwinter = ZCASH_USING_OVERWINTER_SAPLING;
+                }
+            }
+            context.overwinterSignReady = 0;
+            context.segwitParsedOnce = 0;
+            // Initialize for screen pairing
+            memset(&context.tmpCtx.output, 0,
+                      sizeof(context.tmpCtx.output));
+            context.tmpCtx.output.changeAccepted = 1;
+            // Reset segwitWarningSeen flag to prevent displaying the warning for each
+            // segwit input when coontinuing from a previous session (P2=0x80)
+            context.segwitWarningSeen = 0;
+        }
+    } else if (p2 != P2_CONTINUE) {
+        return io_send_sw(SW_INCORRECT_P1_P2);
+    }
+
+    // In segwit mode, warn user one time only to update its client wallet...
+    if (context.usingSegwit
+        && !context.segwitWarningSeen
+        && (p1 == P1_NEXT)
+        && (p2 != P2_CONTINUE)
+        // ...if input is not passed as a TrustedInput
+        && IS_INPUT()
+        && !IS_INPUT_TRUSTED())
+    {
+        if(G_called_from_swap){
+            /* There is no point in displaying a warning when the app is signing
+            in silent mode, as its UI is hidden behind the exchange app*/
+            return io_send_sw(SW_SWAP_WITHOUT_TRUSTED_INPUTS);
+        }
+        context.segwitWarningSeen = 1;
+        request_segwit_input_approval();
+        // Start parsing of the 1st chunk
+        context.transactionBufferPointer = (uint8_t*) buffer->ptr;
+        context.transactionDataRemaining = buffer->size;
+
+        transaction_parse(PARSE_MODE_SIGNATURE);
+        return 0;
+    }
+
+    // Start parsing of the 1st chunk
+    context.transactionBufferPointer = (uint8_t*) buffer->ptr;
+    context.transactionDataRemaining = buffer->size;
+
+    transaction_parse(PARSE_MODE_SIGNATURE);
+
+    return io_send_response_pointer(G_io_apdu_buffer, context.outLength, SW_OK);
+}